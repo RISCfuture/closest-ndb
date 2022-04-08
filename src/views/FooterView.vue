ad1097e9514403a8fb92ca797fefe4f36739644e
Tim Morgan <git@timothymorgan.info>
2022-04-08 00:44:59 -0700

    Add GitHub link

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -2,7 +2,8 @@
   <footer>
     <p>This very useful website was written by <a href="https://tim.codes">Tim Morgan</a> following
       a luminous flash of inspiration. (Actually, it mostly exists just so I can watch the number go
-      up as more and more NDBs are decommissioned.)</p>
+      up as more and more NDBs are decommissioned.) Source code on
+      <a href="https://github.com/RISCfuture/closest-ndb">GitHub</a>.</p>
   </footer>
 </template>
 
