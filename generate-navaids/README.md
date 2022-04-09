d5954be6c1192dc067c2c26e168811936c7b8286
Tim Morgan <git@timothymorgan.info>
2022-04-08 22:31:06 -0700

    * Add script for generating ndbs.json * Remove decommissioned NDBs

---
+++ generate-navaids/README.md
@@ -0,0 +1,7 @@
+# generate-navaids
+
+Generates the `ndbs.json` file from FAA NASR data. Simply run it with
+`swift run` and copy the generated `ndbs.json` file to the appropriate place.
+
+This script will download the current NASR distribution if it does not already
+exist on disk.
---
+++ generate-navaids/Sources/generate-navaids/generate-navaids.swift
@@ -0,0 +1,41 @@
+import Foundation
+import Darwin
+import SwiftNASR
+
+@main
+struct GenerateNavaids {
+    static var workingURL: URL { URL(fileURLWithPath: FileManager.default.currentDirectoryPath) }
+    static var distributionURL: URL { workingURL.appendingPathComponent("distribution.zip") }
+    static var distributionPath: String { distributionURL.path }
+    static var outputURL: URL { workingURL.appendingPathComponent("ndbs.json") }
+    
+    static func main() async throws {
+        let nasr = FileManager.default.fileExists(atPath: distributionPath) ?
+            NASR.fromLocalArchive(distributionURL) :
+            NASR.fromInternetToFile(distributionURL)!
+
+        let _ = try await nasr.load()
+        let navaids = try await nasr.parseNavaids(errorHandler: {
+            fputs($0.localizedDescription + "\n", stderr)
+            return true
+        })
+        
+        var jsonDict = Array<Dictionary<String, Encodable>>()
+        for navaid in navaids {
+            if !navaid.isNDB || !navaid.isOperational { continue }
+            guard let freq = navaid.frequency else { continue }
+            
+            jsonDict.append([
+                "id": navaid.ID,
+                "name": navaid.name,
+                "lat": navaid.position.latitude,
+                "lon": navaid.position.longitude,
+                "freq": String(freq/1000)
+            ])
+        }
+        jsonDict.sort(by: { ($0["id"] as! String) < ($1["id"] as! String) })
+        
+        let json = try JSONSerialization.data(withJSONObject: jsonDict, options: [.prettyPrinted, .sortedKeys])
+        try json.write(to: outputURL)
+    }
+}
